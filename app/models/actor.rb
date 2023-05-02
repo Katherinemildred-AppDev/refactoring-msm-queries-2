6e9edc8f6455ab21d7449d186648457ae055814b
Katherine O'Connell <koconnel@chicagobooth.edu>
2023-05-02 16:09:53 +0000

    successfully refactored

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -21,16 +21,18 @@ class Actor < ApplicationRecord
    # return the_many
  # end
 
-  def filmography
-    the_many = Array.new
+  #def filmography
+    #the_many = Array.new
+  has_many(:characters)
+  has_many(:filmography, {:through=>"characters", :source=>"movie"})
+  
+  #  self.characters.each do |joining_record|
+    #  destination_record = joining_record.movie
 
-    self.characters.each do |joining_record|
-      destination_record = joining_record.movie
+    #  the_many.push(destination_record)
+   # end
 
-      the_many.push(destination_record)
-    end
-
-    return the_many
-  end
+   # return the_many
+ # end
 
 end
